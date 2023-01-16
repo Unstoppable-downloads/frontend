869494d49dfdfecd8b31445e738c38a48e303cc0
Léo OTSHUDI <leootshudi@gmail.com>
2023-01-16 23:44:22 +0100

    Upload form

--- src/components/Upload/SendForm.jsx
+++ src/components/Upload/SendForm.jsx
@@ -28,6 +28,8 @@ const SendForm = () => {
   const { isLoading, setIsLoading, addressTo, setAddressTo, step, setStep, price, setPrice, message, setMessage, selectedFiles, setSelectedFiles, checkFileAvailability, setIsAvailable } = useContext(AceContext);
   const inputFile = useRef(null);
   const [isAFile, setIsAFile] = useState(false);
+  const [selectedValueList, setSelectedValueList] = useState("");
+  const [description, setDescription] = useState("");
 
   const BEGINNING_PROCESS = 0;
   const ENCRYPTING_FILE = 1;
@@ -47,6 +49,14 @@ const SendForm = () => {
       if (IS_DEBUG) console.log(selectedFiles[i]);
     }
   };
+
+  const handleChangeList = (e) => {
+    setSelectedValueList(e.target.value)
+  }
+
+  const handleChangeDescr = (e) => {
+      setDescription(e.target.value);
+  }
 
   var optimistic = false;
   const handleChecked = () => {
@@ -149,127 +159,77 @@ const SendForm = () => {
       <Modal id="sendform-modal" onModalClose={modalCloseHandler} />
       <form>
         <div className="mr-8 flex w-80 flex-col rounded-2xl bg-iexwhite px-4 py-4 text-iexblk shadow-xl">
-          <div className="uploader">
-            {isAFile ? (
-              <div>You are sending:
-                {selectedFiles.slice(0, 1).map((file) => {
-                  return (
-                    <div className="block" key={file.name}>
-                      {file.name}
-                    </div>
-                  );
-                })}
-                <button
-                  className="flex w-full items-center border-b border-gray-500 px-4 py-8"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    inputFile.current.click();
-                  }}
-                  onDrop={(e) => {
-                    e.preventDefault();
-                    handleChange(e);
-                  }}
-                  onDragOver={(e) => {
-                    e.preventDefault();
-                    handleChange(e);
-                  }}
-                >
-                  <svg viewBox="0 0 72 72" className="w-9">
-                    <path
-                      d="M36.493 72C16.118 72 0 55.883 0 36.493 0 16.118 16.118 0 36.493 0 55.882 0 72 16.118 72 36.493 72 55.882 55.883 72 36.493 72zM34 34h-9c-.553 0-1 .452-1 1.01v1.98A1 1 0 0 0 25 38h9v9c0 .553.452 1 1.01 1h1.98A1 1 0 0 0 38 47v-9h9c.553 0 1-.452 1-1.01v-1.98A1 1 0 0 0 47 34h-9v-9c0-.553-.452-1-1.01-1h-1.98A1 1 0 0 0 34 25v9z"
-                      fill="rgb(252 209 90)"
-                      fillRule="nonzero"
-                    ></path>
-                  </svg>
-                  <h3 className="mx-4 text-2xl font-light">Upload files</h3>
-                </button>
-                <input
-                  type="file"
-                  className="hidden"
-                  onChange={handleChange}
-                  ref={inputFile}
-                />
-              </div>
-            ) : (
-              <div>
-                <button
-                  className="flex w-full items-center border-b border-gray-500 px-4 py-16"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    inputFile.current.click();
-                  }}
-                  onDrop={(e) => {
-                    e.preventDefault();
-                    handleChange(e);
-                  }}
-                  onDragOver={(e) => {
-                    e.preventDefault();
-                    handleChange(e);
-                  }}
-                >
-                  <svg viewBox="0 0 72 72" className="w-9">
-                    <path
-                      d="M36.493 72C16.118 72 0 55.883 0 36.493 0 16.118 16.118 0 36.493 0 55.882 0 72 16.118 72 36.493 72 55.882 55.883 72 36.493 72zM34 34h-9c-.553 0-1 .452-1 1.01v1.98A1 1 0 0 0 25 38h9v9c0 .553.452 1 1.01 1h1.98A1 1 0 0 0 38 47v-9h9c.553 0 1-.452 1-1.01v-1.98A1 1 0 0 0 47 34h-9v-9c0-.553-.452-1-1.01-1h-1.98A1 1 0 0 0 34 25v9z"
-                      fill="rgb(252 209 90)"
-                      fillRule="nonzero"
-                    ></path>
-                  </svg>
-                  <h3 className="mx-4 text-2xl font-extralight">
-                    Upload files
-                  </h3>
-                </button>
-                <input
-                  type="file"
-                  className="hidden"
-                  onChange={handleChange}
-                  ref={inputFile}
-                  required
-                />
-              </div>
-            )}
-          </div>
-          <div className="p-4">
-            <div className="pb-4">
+          <div className="flex flex-col">
+            <div>
+              <button
+                className="border border-iexblk w-full rounded-md py-2"
+                data-tip="Click here to choose the file to transfer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  inputFile.current.click();
+                }}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  handleChange(e);
+                }}
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  handleChange(e);
+                }}
+              >
+                Upload
+              </button>
               <input
-                className="w-full border-b border-gray-500 bg-iexwhite pb-2 focus:outline-none"
-                type="text"
-                autoComplete="off"
-                value={addressTo}
-                onChange={(e) => setAddressTo(e.target.value.toLocaleLowerCase())}
-                placeholder="To"
-              />
+                      type="file"
+                      className="hidden"
+                      onChange={handleChange}
+                      ref={inputFile}
+                      required
+                    />
             </div>
-            <div className="pb-4">
-              <input
-                className="w-full border-b border-gray-500 bg-iexwhite pb-2 focus:outline-none"
-                type="text"
-                autoComplete="off"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                placeholder="Message"
-              />
-            </div>
-            <div className="invisible-element">
-              <input
-                className="w-full border-b border-gray-500 bg-iexwhite pb-2 focus:outline-none"
-                type="number"
-                autoComplete="off"
-                value={price}
-                min="0"
-                step="0.01"
-                onChange={(e) => setPrice(e.target.value)}
-                placeholder="Price (RLC)"
-              />
-            </div>
+            
+            <select className="my-4 border border-iexblk" onChange={(e) => handleChangeList(e)}>
+              <option value="music">Music</option>
+              <option value="movie">Movie</option>
+              <option value="series">Series</option>
+              <option value="ebook">E-book</option>
+              <option value="software">Software</option>
+              <option value="game">Game</option>
+              <option value="other">Other file</option>
+            </select>
+
+            <textarea
+              value={description}
+              onChange={handleChangeDescr}
+              placeholder="Description"
+            />
           </div>
           <div className="formFooter mx-auto items-center p-4">
             <ReactTooltip multiline="true" />
 
-            <div className="mb-4 optimisticContainer">
-              <input type="checkbox" name="optimistic" id="optimistic" onClick={handleChecked} />
+            <div className="optimisticContainer mb-4">
+              <input
+                type="checkbox"
+                name="optimistic"
+                id="optimistic"
+                onClick={handleChecked}
+              />
               <label htmlFor="optimistic" className="ml-2">
-                Optimistic IPFS upload <svg data-tip="Activate this setting to speedup the upload process.<br/>By default, the system will ensure that the file is available on IPFS before proceeding to the next step." xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inlineText">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                Optimistic IPFS upload{" "}
+                <svg
+                  data-tip="Activate this setting to speedup the upload process.<br/>By default, the system will ensure that the file is available on IPFS before proceeding to the next step."
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="inlineText h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                  />
                 </svg>
               </label>
             </div>
@@ -281,16 +241,13 @@ const SendForm = () => {
                 onClick={async (e) => {
                   e.preventDefault();
 
-                  publishFile()  ;
+                  publishFile();
 
-                  alert("done")  ;
-
+                  alert("done");
                 }}
               >
                 Transfer
               </button>
-
-
             </div>
           </div>
         </div>
