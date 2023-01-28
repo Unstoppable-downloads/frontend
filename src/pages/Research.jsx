d700c7aefbf3eef33f0a1599ad312728672ef875
Léo OTSHUDI <leootshudi@gmail.com>
2023-01-28 23:46:26 +0100

    post merge commit

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { AceContext } from "../context/context";
 const APP_NAME = process.env.REACT_APP_NAME;
-const IS_DEBUG = process.env.REACT_APP_IS_DEBUG == "true";
+const IS_DEBUG = process.env.REACT_APP_IS_DEBUG === "true";
 
 function Research() {
   return (
