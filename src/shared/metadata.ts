53b8c01903bc3abde874d157d45782de824ee844
m0rpheus <fotshudi@gmail.com>
2023-01-28 10:03:30 +0100

    metadata class update

--- src/shared/metadata.ts
+++ src/shared/metadata.ts
@@ -1,9 +1,11 @@
 
 export default class MetaData{
+    title: string
     fileName: string
     uid:string
     fileSize: number
     chunks: Array<FileChunk>
     hash: string
-    categories: Array<string>
+    categories: Array<string>,
+    description: string
 }
