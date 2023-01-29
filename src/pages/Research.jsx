c8e2af258a0685ffb36e252603f078de7239f4e3
Léo OTSHUDI <leootshudi@gmail.com>
2023-01-29 17:23:51 +0100

    Sidebar init

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -13,37 +13,46 @@ function Research() {
       <Helmet>
         <title>{APP_NAME} | Home</title>
       </Helmet>
-      <div className="relative mx-m flex flex-col py-m">
-        {isSearching ? (
-          <div className="mb-12 flex w-full">
-            <ResearchBar />
-          </div>
-        ) : (
-          <div className="flex w-full">
-            <ResearchBar />
-          </div>
-        )}
+      <div className="flex">
+        <div className="relative mx-m flex w-3/4 flex-col py-m">
+          {isSearching ? (
+            <div className="mb-12 flex w-full">
+              <ResearchBar />
+            </div>
+          ) : (
+            <div className="flex w-full">
+              <ResearchBar />
+            </div>
+          )}
 
-        {isSearching && (
-          <table className="container mt-12 w-full max-w-full table-auto border-collapse">
-            <thead>
-              <tr>
-                <th className="text-center">Name</th>
-                <th className="text-center">Description</th>
-                <th className="text-center">Categories</th>
-                <th className="text-center">IMDB</th>
-                <th className="text-center">Size</th>
-                <th className="px-8 text-center">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-center">
-                <td colSpan={6}>No sent item found.</td>
-              </tr>
-            </tbody>
-          </table>
-        )}
-        <div className="text-iexwhite"></div>
+          {isSearching && (
+            <table className="container mt-12 w-full max-w-full table-auto border-collapse">
+              <thead>
+                <tr>
+                  <th className="text-center">Title</th>
+                  <th className="text-center">Filename</th>
+                  <th className="text-center">Categories</th>
+                  <th className="text-center">Size</th>
+                  <th className="text-center"># DL</th>
+                  <th className="px-8 text-center">&nbsp;</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="text-center">
+                  <td colSpan={6}>No sent item found.</td>
+                </tr>
+              </tbody>
+            </table>
+          )}
+          <div className="text-iexwhite"></div>
+        </div>
+        <aside>
+          <div className="flex flex-col bg-sidebar w-max">
+            <div className="w-full">
+                <h4 className="text-xl">Recent Uploads</h4>
+            </div>
+          </div>
+        </aside>
       </div>
     </>
   );
